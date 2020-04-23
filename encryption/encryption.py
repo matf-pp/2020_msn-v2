50042e37325e3a621ef0323f339ee4b79681460f
Mirko07998 <50887257+Mirko07998@users.noreply.github.com>
2020-04-23 18:59:05 +0200

    Added parts for encryption

---
+++ encryption/encryption.py
@@ -0,0 +1,112 @@
+from base64 import b64encode, b64decode
+import hashlib
+from Cryptodome.Cipher import AES
+import os
+from Cryptodome.Random import get_random_bytes
+
+#I part
+
+def encrypt (plain_text, password):
+    salt = get_random_bytes(AES.block_size)
+    
+    private_key = hashlib.scrypt(password.encode(), salt = salt, n=2**14, r=8, p=1, dklen=32)
+
+    cipher_config = AES.new(private_key, AES.MODE_GCM)
+
+    cipher_text, tag = cipher_config.encrypt_and_digest(bytes(plain_text, 'utf-8'))
+
+    return {
+        'cipher_text': b64encode(cipher_text).decode('utf-8'),
+        'salt': b64encode(salt).decode('utf-8'),
+        'nonce' : b64encode(cipher_config.nonce).decode('utf-8'),
+        'tag': b64encode(tag).decode('utf-8')
+    }
+
+
+def decrypt(enc_dict, password):
+    salt = b64decode(enc_dict['salt'])
+    cipher_text = b64decode(enc_dict['cipher_text'])
+    nonce = b64decode(enc_dict['nonce'])
+    tag = b64decode(enc_dict['tag'])
+
+
+    private_key = hashlib.scrypt(
+        password.encode(), salt = salt, n=2**14, r=8, p=1, dklen=32
+    )
+
+    cipher = AES.new(private_key, AES.MODE_GCM, nonce = nonce)
+
+    decrypted = cipher.decrypt_and_verify(cipher_text, tag)
+
+    return decrypted
+
+#For test
+def main():
+    password = input("Password: ")
+
+    encrypted = encrypt("Mirko lagano AES :D", password)
+    print(encrypted)
+
+    decrypted = decrypt(encrypted, password)
+    print(bytes.decode(decrypted))
+
+
+#II part
+
+
+P = 2 ** 255 - 19
+A = 486662
+def expmod(b, e, m):
+    if e == 0:
+         return 1
+    t = expmod(b, e // 2, m) ** 2 % m
+    if e & 1:
+         t = (t * b) % m
+    return t
+
+def inv(x):
+    return expmod(x, P - 2, P)
+
+def add(xnzn, xmzm, xdzd):
+    xn, zn = xnzn
+    xm, zm = xmzm
+    xd, zd = xdzd
+    x = 4 * (xm * xn - zm * zn) ** 2 * zd
+    z = 4 * (xm * zn - zm * xn) ** 2 * xd
+    return (x % P, z % P)
+
+def double(xnzn):
+    xn, zn = xnzn
+    x = (xn ** 2 - zn ** 2) ** 2
+    z = 4 * xn * zn * (xn ** 2 + A * xn * zn + zn ** 2)
+    return (x % P, z % P)
+
+def curve25519(n, base=9):
+    one = (base,1)
+    two = double(one)
+    # f(m) evaluates to a tuple
+    # containing the mth multiple and the
+    # (m+1)th multiple of base.
+    def f(m):
+        if m == 1: 
+            return (one, two)
+        (pm, pm1) = f(m // 2)
+        if (m & 1):
+            return (add(pm, pm1, one), double(pm1))
+        return (double(pm), add(pm, pm1, one))
+    ((x,z), _) = f(n)
+    return (x * inv(z)) % P
+
+import random
+
+def genkey(n=0):
+    n = n or random.randint(0,P)
+    n &= ~7
+    n &= ~(128 << 8 * 31)
+    n |= 64 << 8 * 31
+    return n
+
+def dh_test():
+    sk1 = genkey()
+    sk2 = genkey()
+    return curve25519(sk1, curve25519(sk2)) == curve25519(sk2, curve25519(sk1))
